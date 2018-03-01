87e6e982f79749da6f1b453b6d5e90aea22cb774
Vladyslav Nesterenko <easterenko@gmail.com>
2018-03-01 15:04:03 +0200

    Add boilerplate code

--- www/js/index.js
+++ www/js/index.js
@@ -16,6 +16,41 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+
+
+// This example displays a marker at the center of Australia.
+// When the user clicks the marker, an info window opens.
+
+function initMap() {
+  var uluru = {lat: -25.363, lng: 131.044};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 4,
+    center: uluru
+  });
+
+  var contentString = `<div id="content">
+    <div id="siteNotice">
+      </div>
+      <h1 id="firstHeading" class="firstHeading">Uluru</h1>
+      <div id="bodyContent"><p>
+      </p></div>
+    </div>`
+
+  var infowindow = new google.maps.InfoWindow({
+    content: contentString
+  });
+
+  var marker = new google.maps.Marker({
+    position: uluru,
+    map: map,
+    title: 'Uluru (Ayers Rock)'
+  });
+  marker.addListener('click', function() {
+    infowindow.open(map, marker);
+  });
+}
+
+
 let app = {
   // Application Constructor
   initialize: function () {
