f6cc605f2bf7c8ccafe6bfeddf1d0b945048bd51
LeraB <kudravka13@gmail.com>
2018-03-02 01:19:18 +0200

    Minor fix on map

---
+++ platforms/windows/www/js/index.js
@@ -0,0 +1,77 @@
+﻿/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+
+// This example displays a marker at the center of Australia.
+// When the user clicks the marker, an info window opens.
+
+function initMap() {
+  var uluru = {lat: -25.363, lng: 131.044};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 4,
+    center: uluru
+  });
+
+  var contentString = `<div id="content">
+    <div id="siteNotice">
+      </div>
+      <h1 id="firstHeading" class="firstHeading">Uluru</h1>
+      <div id="bodyContent"><p>
+      </p></div>
+    </div>`
+
+  var infowindow = new google.maps.InfoWindow({
+    content: contentString
+  });
+
+  var marker = new google.maps.Marker({
+    position: uluru,
+    map: map,
+    title: 'Uluru (Ayers Rock)'
+  });
+  marker.addListener('click', function() {
+    infowindow.open(map, marker);
+  });
+}
+
+
+let app = {
+  // Application Constructor
+  initialize: function () {
+    // document.addEventListener('deviceready', this.onDeviceReady.bind(this), false)
+    this.receivedEvent.call(this)
+  },
+
+  // deviceready Event Handler
+  //
+  // Bind any cordova events here. Common events are:
+  // 'pause', 'resume', etc.
+  onDeviceReady: function () {
+    this.receivedEvent('deviceready')
+  },
+
+  // Update DOM on a Received Event
+  receivedEvent: function (id) {
+    let parentElement = document.getElementById(id)
+    let map = document.getElementById('map')
+    // new google.maps.Map(map)
+  }
+}
+
+app.initialize()
