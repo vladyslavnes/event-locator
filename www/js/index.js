e99f0c50df8be4ccadf3ffb36c931ef4845a7607
LeraB <kudravka13@gmail.com>
2018-03-02 01:59:09 +0200

    Added classes for pop-up

--- www/js/index.js
+++ www/js/index.js
@@ -107,29 +107,41 @@
 // app.initialize()
 var map;
 function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 2,
-    center: new google.maps.LatLng(2.8,-187.3),
-    mapTypeId: 'terrain'
-  });
+    map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 2,
+        center: new google.maps.LatLng(2.8, -187.3),
+        mapTypeId: 'terrain'
+    });
 
-  // Create a <script> tag and set the USGS URL as the source.
-  var script = document.createElement('script');
-  // This example uses a local copy of the GeoJSON stored at
-  // http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojsonp
-  script.src = 'https://developers.google.com/maps/documentation/javascript/examples/json/earthquake_GeoJSONP.js';
-  document.getElementsByTagName('head')[0].appendChild(script);
+    // Create a <script> tag and set the USGS URL as the source.
+    var script = document.createElement('script');
+    // This example uses a local copy of the GeoJSON stored at
+    // http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojsonp
+    script.src = 'https://developers.google.com/maps/documentation/javascript/examples/json/earthquake_GeoJSONP.js';
+    document.getElementsByTagName('head')[0].appendChild(script);
+
+    google.maps.event.addListener(map, "dblclick", function (e) {
+        var clickPosition = e.latLng;
+        $('#pop-up').addClass('displayB').removeClass('displayNone');
+    });
+
+    google.maps.event.addListener(map, "click", function (e) {
+        var clickPosition = e.latLng;
+       // $('#pop-up').attr('display', 'none');
+    });
+
 }
 
 // Loop through the results array and place a marker for each
 // set of coordinates.
-window.eqfeed_callback = function(results) {
-  for (var i = 0; i < results.features.length; i++) {
-    var coords = results.features[i].geometry.coordinates;
-    var latLng = new google.maps.LatLng(coords[1],coords[0]);
-    var marker = new google.maps.Marker({
-      position: latLng,
-      map: map
-    });
-  }
+window.eqfeed_callback = function (results) {
+    for (var i = 0; i < results.features.length; i++) {
+        var coords = results.features[i].geometry.coordinates;
+        var latLng = new google.maps.LatLng(coords[1], coords[0]);
+        var marker = new google.maps.Marker({
+            position: latLng,
+            map: map
+        });
+    }
 }
+
