dc0d16e35513b7dc43c36d4bfbd980dc0ea641f7
LeraB <kudravka13@gmail.com>
2018-03-03 00:02:25 +0200

    Fixed request

--- platforms/browser/www/js/index.js
+++ platforms/browser/www/js/index.js
@@ -16,95 +16,7 @@
 //  * specific language governing permissions and limitations
 //  * under the License.
 //  */
-//
-//
-// // This example displays a marker at the center of Australia.
-// // When the user clicks the marker, an info window opens.
-//
-// function initMap() {
-//   var map = new google.maps.Map(document.getElementById('map'), {
-//     zoom: 3,
-//     center: {lat: 0, lng: 0},
-//     mapTypeId: 'terrain'
-//   });
-//
-//   // Create a <script> tag and set the USGS URL as the source.
-//   var script = document.createElement('script');
-//   // This example uses a local copy of the GeoJSON stored at
-//   // http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojsonp
-//   script.src = 'https://developers.google.com/maps/documentation/javascript/examples/json/earthquake_GeoJSONP.js';
-//   document.getElementsByTagName('head')[0].appendChild(script);
-// }
-//
-// // Loop through the results array and place a marker for each
-// // set of coordinates.
-// window.eqfeed_callback = function(results) {
-//   for (var i = 0; i < results.features.length; i++) {
-//     var coords = results.features[i].geometry.coordinates;
-//     var latLng = new google.maps.LatLng(coords[1],coords[0]);
-//     var marker = new google.maps.Marker({
-//       position: latLng,
-//       map: map
-//     });
-//   }
-// }
-//
-//   var coordsDiv = document.getElementById('coords');
-//   map.controls[google.maps.ControlPosition.TOP_CENTER].push(coordsDiv);
-//   map.addListener('mousemove', function(event) {
-//     coordsDiv.textContent =
-//         'lat: ' + Math.round(event.latLng.lat()) + ', ' +
-//         'lng: ' + Math.round(event.latLng.lng());
-//   });
-//
-//
-//   var contentString = `<div id="content">
-//     <div id="siteNotice">
-//       </div>
-//       <h1 id="firstHeading" class="firstHeading"></h1>
-//       <div id="bodyContent"><p>
-//       </p></div>
-//     </div>`
-//
-//   var infowindow = new google.maps.InfoWindow({
-//     content: contentString
-//   });
-//
-//   var marker = new google.maps.Marker({
-//     position: uluru,
-//     map: map,
-//     title: 'Uluru (Ayers Rock)'
-//   });
-//   marker.addListener('click', function() {
-//     infowindow.open(map, marker);
-//   });
-//
-//
-//
-// let app = {
-//   // Application Constructor
-//   initialize: function () {
-//     // document.addEventListener('deviceready', this.onDeviceReady.bind(this), false)
-//     this.receivedEvent.call(this)
-//   },
-//
-//   // deviceready Event Handler
-//   //
-//   // Bind any cordova events here. Common events are:
-//   // 'pause', 'resume', etc.
-//   onDeviceReady: function () {
-//     this.receivedEvent('deviceready')
-//   },
-//
-//   // Update DOM on a Received Event
-//   receivedEvent: function (id) {
-//     let parentElement = document.getElementById(id)
-//     let map = document.getElementById('map')
-//     // new google.maps.Map(map)
-//   }
-// }
-//
-// app.initialize()
+
 var map;
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
@@ -115,14 +27,25 @@ function initMap() {
 
     // Create a <script> tag and set the USGS URL as the source.
     var script = document.createElement('script');
-    // This example uses a local copy of the GeoJSON stored at
-    // http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojsonp
-    script.src = 'https://developers.google.com/maps/documentation/javascript/examples/json/earthquake_GeoJSONP.js';
-    document.getElementsByTagName('head')[0].appendChild(script);
+
+    var contentString = '<div id="content">'+
+        '<div id="siteNotice">'+
+        '</div>'+'<span>'+CreatedBy+'</span>'
+        '<h1 id="firstHeading" class="firstHeading">Name event'+name+'</h1>'+
+           '<div class="time">'+time+'</div>'
+        '<div id="bodyContent">'+
+        '<p>'+description+'</p>'+
+        '</div>'+
+        '</div>';
+
+    var infowindow = new google.maps.InfoWindow({
+        content: contentString
+    });
+
 
     google.maps.event.addListener(map, "dblclick", function (e) {
         var clickPosition = e.latLng;
-        $('#pop-up').addClass('displayB').removeClass('displayNone');
+        $('#pop-up').removeClass('displayNone').addClass('displayB');
     });
 
     google.maps.event.addListener(map, "click", function (e) {
@@ -134,14 +57,14 @@ function initMap() {
 
 // Loop through the results array and place a marker for each
 // set of coordinates.
+
 window.eqfeed_callback = function (results) {
     for (var i = 0; i < results.features.length; i++) {
-        var coords = results.features[i].geometry.coordinates;
-        var latLng = new google.maps.LatLng(coords[1], coords[0]);
+        var coords = results.features[i].geometry.coords;
+        var latLng = new google.maps.LatLng(coords[lat], coords[lng]);
         var marker = new google.maps.Marker({
             position: latLng,
             map: map
         });
     }
 }
-
